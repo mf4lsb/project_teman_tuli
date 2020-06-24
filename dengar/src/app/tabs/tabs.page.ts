069905a99e6cfb51e79b321b29712b35b13e0690
mf4lsb <msn.bahri@gmail.com>
2020-06-24 23:29:27 +0700

    update front-end

--- dengar/src/app/tabs/tabs.page.ts
+++ dengar/src/app/tabs/tabs.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: './tabs.page.html',
-  styleUrls: ['./tabs.page.scss'],
+  selector: "app-tabs",
+  templateUrl: "./tabs.page.html",
+  styleUrls: ["./tabs.page.scss"],
 })
 export class TabsPage implements OnInit {
+  tabTuliIcon: String;
+  tabDengarIcon: String;
+  tab: String;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  didChangeTab(ev: any) {
+    if (ev["tab"] == "teman-tuli") {
+      this.tabTuliIcon = "../../assets/icon/ear-white.svg";
+      this.tabDengarIcon = "../../assets/icon/hand-blue.svg";
+      this.tab = "teman-tuli";
+    } else {
+      this.tabTuliIcon = "../../assets/icon/ear-blue.svg";
+      this.tabDengarIcon = "../../assets/icon/hand-white.svg";
+      this.tab = "teman-dengar";
+    }
   }
-
 }
