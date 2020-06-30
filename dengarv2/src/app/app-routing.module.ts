5555edcaeb638a8b705f5c4ef282d0c29c62e9e4
Aloysiusagias <aloysiusagias@gmail.com>
2020-06-30 23:55:44 +0700

    Update

--- dengarv2/src/app/app-routing.module.ts
+++ dengarv2/src/app/app-routing.module.ts
@@ -3,14 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'splash',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
+
 ];
 
 @NgModule({
